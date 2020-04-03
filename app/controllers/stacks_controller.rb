12226a952d208b14b6ef0529d012d93468b6c8e2
alhimowicz <alhimowicz@gmail.com>
2020-04-03 20:22:48 +0300

    Added sessions

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -19,7 +19,8 @@ class StacksController < ApplicationController
         @stack = Stack.new(create_params)
         
         if @stack.save
-            redirect_to @stack
+            session[:current_stack_id] = @stack.id
+            redirect_to @stack, notice: "Stack has been created"
         else
             render 'new'
         end
@@ -27,19 +28,25 @@ class StacksController < ApplicationController
 
     def update
         @stack = Stack.find params[:id]
-
-        if @stack.update update_params
-            redirect_to @stack
+        if session[:current_stack_id] == @stack.id
+            if @stack.update update_params
+                redirect_to @stack, notice: "Stack has been updated"
+            else
+                render 'edit'
+            end
         else
-            render 'edit'
+            render 'edit', notice: "You have no rights to update this stack"
         end
     end
 
     def destroy
         @stack = Stack.find(params[:id])
-        @stack.destroy
-
-        redirect_to stacks_path
+        if session[:current_stack_id] == @stack.id
+            @stack.destroy
+            redirect_to stacks_path
+        else
+            redirect_to stacks_path, notice: "You have no rights to delete this stack"
+        end 
     end
 
     private
